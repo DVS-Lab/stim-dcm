a300e2c94362e27480bca258fb280d171f38c48a
jameswyngaarden <james.wyngaarden@temple.edu>
2023-10-18 15:32:41 -0400

    fix confounds

--- code/run_spm.m
+++ code/run_spm.m
@@ -10,20 +10,26 @@ spm fmri
 %RewardVlpfcRun1 = import('/ZPOOL/data/projects/stim-dcm/derivatives/spm/EVFiles/sub-189/cardgame/_reward_vlpfc.txt');
 %RewardVlpfcRun1(:,1)
 
-sub189_run1=readtable("/ZPOOL/data/projects/stim-dcm/derivatives/spm/confounds/sub-189/sub-189_task-cardgame_run-1_desc-fslConfounds.tsv", "FileType","text","Delimiter","\t");
-for i=1:size(sub189_run1(1,:),2)
-    sub189_run1.Properties.VariableNames(i) = "R"+i;
+R=readtable("/ZPOOL/data/projects/stim-dcm/derivatives/spm/confounds/sub-189/sub-189_task-cardgame_run-1_desc-fslConfounds.tsv", "FileType","text","Delimiter","\t");
+for i=1:size(R(1,:),2)
+    R.Properties.VariableNames(i) = "R"+i;
+    R=table2array(R);
 end
-save("sub189_run1_confounds.mat", "sub189_run1");
+save("sub189_run1_confounds.mat", "R");
+clear
 
-sub189_run2=readtable("/ZPOOL/data/projects/stim-dcm/derivatives/spm/confounds/sub-189/sub-189_task-cardgame_run-2_desc-fslConfounds.tsv", "FileType","text","Delimiter","\t");
-for i=1:size(sub189_run2(1,:),2)
-    sub189_run2.Properties.VariableNames(i) = "R"+i;
+R=readtable("/ZPOOL/data/projects/stim-dcm/derivatives/spm/confounds/sub-189/sub-189_task-cardgame_run-2_desc-fslConfounds.tsv", "FileType","text","Delimiter","\t");
+for i=1:size(R(1,:),2)
+    R.Properties.VariableNames(i) = "R"+i;
+    R=table2array(R);
 end
-save("sub189_run2_confounds.mat", "sub189_run2");
+save("sub189_run2_confounds.mat", "R");
+clear
 
-sub189_run3=readtable("/ZPOOL/data/projects/stim-dcm/derivatives/spm/confounds/sub-189/sub-189_task-cardgame_run-3_desc-fslConfounds.tsv", "FileType","text","Delimiter","\t");
-for i=1:size(sub189_run3(1,:),2)
-    sub189_run3.Properties.VariableNames(i) = "R"+i;
+R=readtable("/ZPOOL/data/projects/stim-dcm/derivatives/spm/confounds/sub-189/sub-189_task-cardgame_run-3_desc-fslConfounds.tsv", "FileType","text","Delimiter","\t");
+for i=1:size(R(1,:),2)
+    R.Properties.VariableNames(i) = "R"+i;
+    R=table2array(R);
 end
-save("sub189_run3_confounds.mat", "sub189_run3");
+save("sub189_run3_confounds.mat", "R");
+clear
